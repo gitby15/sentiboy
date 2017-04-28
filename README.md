ab42713312690db81dfa59391cfa73104098ce5e
lijin14 <lijin14@meituan.com>
2017-04-28 18:21:47 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sentiboy
 sentry练手程序
+本地代理服务器， 实现思路清参考alloyTeam的[livepool]()和阿里的[anyproxy](https://github.com/alibaba/anyproxy)
+
--- server.js
+++ server.js
@@ -12,40 +12,44 @@ const doc = require('./route/doc.js');
 const net = require('net');
 const url = require('url');
 const http = require('http');
-// 	httpProxy = require('http-proxy');
-// var proxy = httpProxy.createProxyServer();
 
-
-let app = new koa(),
-	router = new koaRouter();
 AV.init({
-	appId: process.env.LEANCLOUD_APP_ID || 'usbJGVbiA5J2WA8s19H9cB1u-gzGzoHsz',
-	appKey: process.env.LEANCLOUD_APP_KEY || 'FL21tj3BQl8gM9bnGwwcja2F',
-	masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'TzFPUEhAVCVEwhRlpxVAeOrl'
-});
-app.use(AV.koa());
-
-router.use('/waimai/ajax', mock);
-router.use('/waimai', doc);
-app.use(router.routes());
-
-app.use(async(ctx)=> {
-	let {
-		originalUrl,
-		socket,
-		req,
-	} = ctx;
-	// let severSocket = net.connect(severUrl.port, severUrl.hostname, function () {
-	//
-	// })
-	console.log(ctx);
-})
-
-app.on('error', (err, ctx)=> {
-	console.log(err);
+  appId: process.env.LEANCLOUD_APP_ID || 'usbJGVbiA5J2WA8s19H9cB1u-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'FL21tj3BQl8gM9bnGwwcja2F',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'TzFPUEhAVCVEwhRlpxVAeOrl'
 });
 
-app.listen(process.env.LEANCLOUD_APP_PORT);
+let requestHandler = function (req, res) {
+  
+}
+let connectReqHandler = function(req,cltSocket,header){
+  
+}
+let proxyPort = 9001;
+function proxyServer(option) {
+  /**
+   * 1. 初始化应用数据
+   * 2. 创建代理服务器
+   * 3. 处理connect请求
+   * 4. 启动代理服务器
+   * 5. 启动web socket服务器
+   * 6. 启动web interface
+   * 7. 设置全局代理
+   * 8. 管理服务器状态
+   */
+  let s = null;// httpProxyServer
+  s = http.createServer(requestHandler);// step1
+  s.on('connect',connectReqHandler);// step2
+  s.listen(proxyPort);// step3
+  // websocket 服务器暂时先不实现
+  // web interface 暂时先不实现
+  // 暂时先不设置全局代理
+  
+  
+}
+
+
+//app.listen(process.env.LEANCLOUD_APP_PORT);
 
 // let httpServer = http.createServer(app);
 //
